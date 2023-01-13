cd28551d89eb22446efa56ec291c1801f7d202a7
dev-gambits <gambits-dev@gmail.com>
2023-01-13 00:31:28 -0300

    Segundo commit

--- src/main/java/cl/springs/springs/Main.java
+++ src/main/java/cl/springs/springs/Main.java
@@ -1,6 +1,13 @@
 package cl.springs.springs;
 
 public class Main {
+    public static void main(String[] args) {
 
+        Integer numero1 = 5;
+        Integer numero2 = 3;
 
+        Integer numeroTotal = numero1 * numero2;
+
+        System.out.println("numeroTotal = " + numeroTotal);
+    }
 }
