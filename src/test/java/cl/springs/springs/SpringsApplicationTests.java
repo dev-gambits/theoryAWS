26b2c428272c465d5debd4b183702044194a214d
Emmanuel-Spring <e.nieto@alumnos.santotomas.cl>
2023-01-12 23:32:16 -0300

    first commit

---
+++ src/test/java/cl/springs/springs/SpringsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.springs.springs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
